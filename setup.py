fccf44dd8559f5e7986189d49b75735ee163d8c0
David Meiklejohn <david@gooligum.com.au>
2016-03-22 08:02:56 +1100

    Added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name 				= 'wombat-support',
+	  version 			= '1.0.0',
+	  author			= 'David Meiklejohn',
+	  author_email		= 'support@gooligum.com',
+	  description		= 'Library to drive Wombat prototyping board for Raspberry Pi',
+	  license			= 'MIT',
+	  url				= 'https://github.com/gooligumelec/wombat-code',
+	  py_modules        = ['wombat'],
+      )
+
